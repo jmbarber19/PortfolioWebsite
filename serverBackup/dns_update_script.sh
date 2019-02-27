132ce2d630356403e4df535dcc4c5122182a70e4
waterbuddy16 <watercalciumandchlorides@gmail.com>
2019-02-27 02:29:50 -0500

    removed backup

--- serverBackup/dns_update_script.sh
+++
@@ -1,6 +0,0 @@
-myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-wget "https://eD83dNUk4pReBKCR:F8ScVJq03njv8Zww@domains.google.com/nic/update?hostname=www.justinbarber.me&myip=${myip}" -O dns_update_results.txt
-printf "\nLast run: `date`\nMy IPv4: ${myip}" >> dns_update_results.txt
-
-#wget https://eD83dNUk4pReBKCR:F8ScVJq03njv8Zww@domains.google.com/nic/update?hostname=www.justinbarber.me -O dns_update_results.txt
-#echo "Last run: `date` " >> dns_update_results.txt
